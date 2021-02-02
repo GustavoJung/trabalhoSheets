8be8b436855cf3c50dd52ca4a748fc3bb5e9a222
GustavoJung <31144620+GustavoJung@users.noreply.github.com>
2021-02-01 22:16:11 -0300

    Add files via upload
    Read e Write

---
+++ src/main/java/com/example/Trabalho/TrabalhoApplication.java
@@ -0,0 +1,19 @@
+package com.example.Trabalho;
+
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.security.GeneralSecurityException;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoApplication.class, args);
+		
+	}
+
+}
